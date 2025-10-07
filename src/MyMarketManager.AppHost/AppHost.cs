cda21213ac45f1e211b1285aead3af8dd3c39fa5
Matthew Leibowitz <mattleibow@live.com>
2025-10-07 05:04:49 +0200

    Added the product pages in the webapp

--- src/MyMarketManager.AppHost/AppHost.cs
+++ src/MyMarketManager.AppHost/AppHost.cs
@@ -4,4 +4,7 @@ var sqlServer = builder.AddAzureSqlServer("sql");
 
 var database = sqlServer.AddDatabase("mymarketmanager");
 
+builder.AddProject<Projects.MyMarketManager_WebApp>("webapp")
+    .WithReference(database);
+
 builder.Build().Run();
--- src/MyMarketManager.WebApp/Program.cs
+++ src/MyMarketManager.WebApp/Program.cs
@@ -1,12 +1,29 @@
+using MyMarketManager.Data;
 using MyMarketManager.WebApp.Components;
+using MyMarketManager.WebApp.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Aspire service defaults (health checks, service discovery, and telemetry)
+builder.AddServiceDefaults();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Add Entity Framework with Aspire
+builder.AddSqlServerDbContext<MyMarketManagerDbContext>("mymarketmanager");
+
+// Add database migration as a hosted service (runs in all environments)
+builder.Services.AddHostedService<DatabaseMigrationService>();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var app = builder.Build();
+
+// Map Aspire default endpoints (health checks)
+app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -15,6 +32,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
